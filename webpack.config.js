1103a589113cd3c8f5ac14b90cc85531adf88446
Jakub Jurkian <jakub@jurkian.com>
2017-05-15 00:44:28 +0200

    Add CSS/Sass loaders and PostCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 const path = require('path');
 const webpack = require('webpack');
 
+// PostCSS loader settings
+let postCSSloader = {
+  loader: 'postcss-loader',
+  options: {
+    plugins: (loader) => [
+      require('autoprefixer')({
+        browsers: [
+          '> 2%',
+          'last 3 versions',
+        ]
+      }),
+      require('cssnano')()
+    ]
+  }
+};
+
 module.exports = {
    entry: {
       main: './src/js/index.js'
@@ -9,6 +25,25 @@ module.exports = {
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: '[name].bundle.js'
+   },
+
+   module: {
+      rules: [{
+         test: /\.css$/,
+         use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' },
+            postCSSloader
+         ]
+      }, {
+         test: /\.scss$/,
+         use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+            postCSSloader
+         ]
+      }]
    },
 
    devServer: {
