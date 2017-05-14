8f2a82e88332db82ca57c0a5f71af25a8a6974a7
Jakub Jurkian <jakub@jurkian.com>
2017-05-15 00:23:00 +0200

    Add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-  
+   entry: {
+      main: './src/js/index.js'
+   },
+
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js'
+   },
+
+   devServer: {
+      contentBase: path.join(__dirname, 'src'),
+      compress: true,
+      port: 8080,
+      stats: 'minimal',
+      open: true
+   },
 };
